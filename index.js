89148d293528a396b3062dfecce55a5d685636e4
suman-java <suman879618@gmail.com>
2026-07-27 22:23:26 +0530

    HTML_Code

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function showDetails() {
+    let details = document.getElementById("details");
+
+    if (details.style.display === "block") {
+        details.style.display = "none";
+    } else {
+        details.style.display = "block";
+    }
+}
+
