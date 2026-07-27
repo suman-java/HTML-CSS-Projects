e77b751f48785c53aef47e837a673755c4be1a9a
suman-java <suman879618@gmail.com>
2026-07-27 22:45:57 +0530

    HTML_Code

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-
 function showDetails() {
+
     let details = document.getElementById("details");
+    let button = document.querySelector("button");
 
-    if (details.style.display === "block") {
+    if(details.style.display === "block"){
+
         details.style.display = "none";
-    } else {
-        details.style.display = "block";
-    }
-}
+        button.innerHTML = "Read More";
 
+    }else{
+
+        details.style.display = "block";
+        button.innerHTML = "Read Less";
+
+    }
+
+}
